de3974d0a7370e075fceb04e3fbc411d0849b832
Bivizul <mamochapa@gmail.com>
2022-06-30 09:05:38 +0200

    - Making UI for Search Fragment, - Load data from server into RecyclerView

--- app/src/main/java/com/bivizul/newsapp/ui/search/SearchFragment.kt
+++ app/src/main/java/com/bivizul/newsapp/ui/search/SearchFragment.kt
@@ -1,24 +1,77 @@
 package com.bivizul.newsapp.ui.search
 
 import android.os.Bundle
-import android.view.LayoutInflater
+import android.util.Log
 import android.view.View
-import android.view.ViewGroup
+import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
+import by.kirich1409.viewbindingdelegate.viewBinding
+import com.bivizul.newsapp.R
 import com.bivizul.newsapp.databinding.FragmentSearchBinding
+import com.bivizul.newsapp.ui.adapters.NewsAdapter
+import com.bivizul.newsapp.utils.Constants
+import com.bivizul.newsapp.utils.Resource
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
-class SearchFragment : Fragment() {
+@AndroidEntryPoint
+class SearchFragment : Fragment(R.layout.fragment_search) {
 
-    private var _binding: FragmentSearchBinding? = null
-    private val binding: FragmentSearchBinding
-        get() = _binding ?: throw RuntimeException("FragmentSearchBinding is null")
+    private val binding by viewBinding(FragmentSearchBinding::bind)
+    private val viewModel by viewModels<SearchViewModel>()
+    lateinit var newsAdapter: NewsAdapter
+    var job: Job? = null
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?,
-    ): View {
-        _binding = FragmentSearchBinding.inflate(layoutInflater, container, false)
-        return binding.root
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initAdapter()
+
+        binding.etSearch.addTextChangedListener { text ->
+            job?.cancel()
+            job = MainScope().launch {
+                delay(500)
+                text?.let {
+                    if (it.toString().isEmpty()) {
+                        viewModel.getSearchNews(query = it.toString())
+                    }
+                }
+            }
+        }
+
+        viewModel.searchNewsLiveData.observe(viewLifecycleOwner) { response ->
+            with(binding) {
+                when (response) {
+                    is Resource.Success -> {
+                        pbPageSearch.visibility = View.INVISIBLE
+                        response.data?.let {
+                            newsAdapter.differ.submitList(it.articles)
+                        }
+                    }
+                    is Resource.Error -> {
+                        pbPageSearch.visibility = View.INVISIBLE
+                        response.data?.let {
+                            Log.d(Constants.TAG, "MainFragment error: $it")
+                        }
+                    }
+                    is Resource.Loading -> {
+                        pbPageSearch.visibility = View.VISIBLE
+                    }
+                }
+            }
+        }
+    }
+
+    private fun initAdapter() {
+        newsAdapter = NewsAdapter()
+        binding.rvSearchNews.apply {
+            adapter = newsAdapter
+            layoutManager = LinearLayoutManager(activity)
+        }
     }
 }
